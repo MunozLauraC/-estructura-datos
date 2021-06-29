58866abcf4094f018bb7bcede382bcb2e0624498
MunozLauraC <lauracris.munoz@gmail.com>
2021-06-29 19:03:32 +0200

    "primer cambio"

---
+++ Clase 1/Programa4/src/App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] parametrosDeConsola){
+        Integer numeroLeido = LeerNumero();
+        float fahrenheit = ComversorDeCelciusAFahrenheit( numeroLeido );
+        System.out.println( "La temperatura en F es: " + fahrenheit );
+    }
+
+    public static float ComversorDeCelciusAFahrenheit(float tCelcius){
+        float temperaturaFahrenheit = 32 + ( 9 * tCelcius / 5 );
+        return temperaturaFahrenheit;
+    }
+
+    public static Integer LeerNumero(){
+        Scanner Lectura= new Scanner(System.in);
+        System.out.println("Por favor ingrese el numero:");
+        int numero=Lectura.nextInt();
+        Lectura.close();
+        return numero;
+    }
+}
